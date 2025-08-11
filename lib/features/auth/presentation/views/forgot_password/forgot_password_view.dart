cabf059f71bc81e343d7dd66d801e8db3f9d8786
Ahmed-Hamdy-911 <ahmedhamdysaad3@gmail.com>
2025-08-11 16:07:54 +0300

    Add and finish the response to forgot password screen

--- lib/features/auth/presentation/views/forgot_password/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password/forgot_password_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jot_do/core/widgets/constants_spaces_widgets.dart';
+import 'package:jot_do/features/auth/presentation/widgets/auth_exited_text.dart';
 import 'package:jot_do/generated/l10n.dart';
 import '../../../../../core/widgets/custom_material_button.dart';
 
@@ -20,48 +21,62 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    S.of(context).reset_password,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: MediaQuery.sizeOf(context).width < 600
-                          ? MediaQuery.sizeOf(context).width * 0.06
-                          : MediaQuery.sizeOf(context).width * 0.05,
-                      color: Colors.black,
-                    ),
+        padding: const EdgeInsets.all(16.0),
+        child: LayoutBuilder(builder: (context, constraints) {
+          if (constraints.maxWidth >= 600) {
+            return Center(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  width: constraints.maxWidth * 0.5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AuthExcitedText(
+                        title: S.of(context).reset_password,
+                        subtitle: S.of(context).send_password_reset,
+                      ),
+                      const LargeSpace(),
+                      Form(
+                        key: _formKey,
+                        child:
+                            CustomEmailField(emailController: _emailController),
+                      ),
+                      const LargeSpace(),
+                      CustomMaterialButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Handle password reset logic
+                          }
+                        },
+                        text: S.of(context).send,
+                      ),
+                    ],
                   ),
-                  const SmallSpace(),
-                  Text(
-                    S.of(context).send_password_reset,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-            const LargeSpace(),
-            Form(
-              key: _formKey,
-              child: CustomEmailField(emailController: _emailController),
-            ),
-            const LargeSpace(),
-            CustomMaterialButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {}
-              },
-              text: S.of(context).send,
-            )
-          ],
-        ),
+            );
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AuthExcitedText(
+                  title: S.of(context).reset_password,
+                  subtitle: S.of(context).send_password_reset),
+              const LargeSpace(),
+              Form(
+                key: _formKey,
+                child: CustomEmailField(emailController: _emailController),
+              ),
+              const LargeSpace(),
+              CustomMaterialButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+                text: S.of(context).send,
+              )
+            ],
+          );
+        }),
       ),
     );
   }
