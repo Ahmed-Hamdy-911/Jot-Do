e70f086b4751c03a58c42729319a48e3d1ddffe7
Ahmed-Hamdy-911 <ahmedhamdysaad3@gmail.com>
2025-08-18 23:32:06 +0300

    Update SlidableActions widget

--- lib/features/home/presentation/views/note/widgets/slidable_actions.dart
+++ lib/features/home/presentation/views/note/widgets/slidable_actions.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import '../../../../../../core/constants/constant.dart';
+import 'package:jot_do/core/constants/constant.dart';
 import '../../../../../../generated/l10n.dart';
 
 ActionPane onDismissibleStartAction(context) => ActionPane(
-        motion: const DrawerMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (context) => null,
-            backgroundColor: const Color(0xFFE53935),
-            foregroundColor: Colors.white,
-            borderRadius: BorderRadius.circular(AppConstants.kRadius),
-            icon: Icons.delete,
-            label: S.of(context).delete,
+      motion: const DrawerMotion(),
+      children: [
+        SlidableAction(
+          onPressed: (context) => null,
+          backgroundColor: const Color(0xFFE53935),
+          foregroundColor: Colors.white,
+          icon: Icons.delete,
+          label: S.of(context).delete,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(AppConstants.kRadius),
+            bottomRight: Radius.circular(AppConstants.kRadius),
           ),
-        ],
-      );
-  ActionPane onDismissibleEndAction(context) => ActionPane(
-        motion: const DrawerMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (context) => null,
-            backgroundColor: const Color(0xFF26A69A),
-            foregroundColor: Colors.white,
-            icon: Icons.push_pin,
-            label: S.of(context).pin,
+        ),
+      ],
+    );
+ActionPane onDismissibleEndAction(context) => ActionPane(
+      motion: const DrawerMotion(),
+      children: [
+        SlidableAction(
+          onPressed: (context) => null,
+          backgroundColor: const Color(0xFF26A69A),
+          foregroundColor: Colors.white,
+          icon: Icons.push_pin,
+          label: S.of(context).pin,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(AppConstants.kRadius),
+            bottomLeft: Radius.circular(AppConstants.kRadius),
           ),
-          SlidableAction(
-            onPressed: (context) => null,
-            backgroundColor: Colors.blueGrey.shade500,
-            foregroundColor: Colors.white,
-            icon: Icons.archive,
-            label: S.of(context).archive,
+        ),
+        SlidableAction(
+          onPressed: (context) => null,
+          backgroundColor: Colors.blueGrey.shade500,
+          foregroundColor: Colors.white,
+          icon: Icons.archive,
+          label: S.of(context).archive,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(AppConstants.kRadius),
+            bottomLeft: Radius.circular(AppConstants.kRadius),
           ),
-        ],
-      );
+        ),
+      ],
+    );
