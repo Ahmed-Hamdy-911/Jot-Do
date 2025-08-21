b31268ebc3ad7689da803b84020f0a2d9f5d945d
Ahmed-Hamdy-911 <ahmedhamdysaad3@gmail.com>
2025-08-21 18:42:58 +0300

    Refactor EmptyWidget to use LayoutBuilder for responsive design

--- lib/core/widgets/empty_widget.dart
+++ lib/core/widgets/empty_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-import 'constants_spaces_widgets.dart';
-
 class EmptyWidget extends StatelessWidget {
   const EmptyWidget({
     super.key,
@@ -11,20 +9,45 @@ class EmptyWidget extends StatelessWidget {
   final String text;
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Lottie.asset("assets/animations/empty.json"),
-        const MediumSpace(),
-        Text(
-          text,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    ));
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(child: Lottie.asset("assets/animations/empty.json")),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ]);
+        } else {
+          return Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  child: Lottie.asset("assets/animations/empty.json")),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.15,
+              )
+            ],
+          ));
+        }
+      },
+    );
   }
 }
