8dd86cb963cb94339a0366f79b03c52faae298f9
Ahmed-Hamdy-911 <ahmedhamdysaad3@gmail.com>
2025-09-03 00:10:17 +0300

    `Added Firebase Authentication plugin and updated dependencies`

---
+++ lib/features/auth/data/repository/auth_repo.dart
@@ -0,0 +1,16 @@
+class AuthRepo {
+
+  
+  // Define your authentication methods here
+  Future<void> login(String email, String password) async {
+    // Implement login logic
+  }
+
+  Future<void> signOut() async {
+    // Implement sign-out logic
+  }
+
+  Future<void> register(String email, String password) async {
+    // Implement registration logic
+  }
+}
